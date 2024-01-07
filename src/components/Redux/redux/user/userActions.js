0a987a4a58fdc227fad8bef38a262b918c31d7e4
Amir Rismaani <amir.rismaani@gmail.com>
2024-01-07 18:44:04 +0330

    add sub action in user actions

--- src/components/Redux/redux/user/userActions.js
+++ src/components/Redux/redux/user/userActions.js
@@ -1,11 +1,26 @@
 import axios from "axios"
 import { FETCH_USER_FAILURE, FETCH_USER_REQUEST, FETCH_USER_SUCCESS } from "./userTypes"
+
+function fetchUserRequest() {
+    return { type: FETCH_USER_REQUEST }
+}
+
+function fetchUserSuccess(data) {
+    return { type: FETCH_USER_SUCCESS, payload: data }
+}
+
+function fetchUserFailure(err) {
+    return { type: FETCH_USER_FAILURE, payload: err.message }
+}
+
 // export const fetchUsers = () => () => {};
 export function fetchUsers() {
     return (dispatch) => {
-        dispatch({ type: FETCH_USER_REQUEST })
+        // dispatch({ type: FETCH_USER_REQUEST })
+        // or
+        dispatch(fetchUserRequest())
         axios.get("https://jsonplaceholder.typicode.com/users")
-            .then(({ data }) => dispatch({ type: FETCH_USER_SUCCESS, payload: data }))
-            .catch(err => dispatch({ type: FETCH_USER_FAILURE, payload: err.message }))
+            .then(({ data }) => dispatch(fetchUserSuccess(data)))
+            .catch(err => dispatch(fetchUserFailure(err)))
     }
 }
